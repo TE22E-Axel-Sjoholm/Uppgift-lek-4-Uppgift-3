01c01c99dc6139e4c7b206656af5ca741aa59386
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-09-20 10:10:50 +0200

    La till jämn ålderräknare, printar hur många år

--- src/App.java
+++ src/App.java
@@ -5,7 +5,6 @@ public class App {
 
         System.out.println("Mata in en följande delar efter varandra med mellanslag mellan varje del: Förnamn Efternamn Längd Vikt Ålder");
         String svar = tb.nextLine();
-        System.out.println(svar);
 
         int space1 = svar.indexOf(" ");
         int space2 = svar.indexOf(" ",(space1+1));
@@ -13,17 +12,26 @@ public class App {
         int space4 = svar.indexOf(" ",(space3+1));
 
         String förnamn = svar.substring(0, space1);
-        String efternamn = svar.substring(space1, space2);
-        String längd = svar.substring(space2, space3);
-        String vikt = svar.substring(space3, space4);
-        String ålder = svar.substring(space4);
+        String efternamn = svar.substring(space1+1, space2);
+        String längd = svar.substring(space2+1, space3);
+        String vikt = svar.substring(space3+1, space4);
+        String ålder = svar.substring(space4+1);
 
 
         System.out.println("Förnamn: "+förnamn);
-        System.out.println("Efternamn:"+efternamn);
-        System.out.println("Längd:"+längd);
-        System.out.println("Vikt:"+vikt);
-        System.out.println("Ålder:"+ålder);
+        System.out.println("Efternamn: "+efternamn);
+        System.out.println("Längd: "+längd);
+        System.out.println("Vikt: "+vikt);
+        System.out.println("Ålder: "+ålder);        
+
+        int närmasteJämntÅlder = (Integer.parseInt(ålder) + 1) / 2 * 2;
+        if((närmasteJämntÅlder-Integer.parseInt(ålder)) == 2){
+            System.out.println("Det är 2 år tills du fyller jämt");
+        } else{
+            System.out.println("Det är "+(närmasteJämntÅlder-Integer.parseInt(ålder))+" år tills du fyller jämt.");
+        }
+
+
 
         tb.close();
     }
